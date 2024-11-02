7a7f121885f93bf33e354ebbbccd18fe607938a6
ABIRAJ-JUNIOR <abirajjr2002@gmail.com>
2024-11-03 00:24:27 +0530

    form designed finished

--- Authentication/src/app/Components/login-component/login-component.component.ts
+++ Authentication/src/app/Components/login-component/login-component.component.ts
@@ -1,12 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-component',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,CommonModule,FormsModule],
   templateUrl: './login-component.component.html',
   styleUrl: './login-component.component.css'
 })
 export class LoginComponentComponent {
+  signinForm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+      this.signinForm = this.formBuilder.group({
+          email: ['', [Validators.required, Validators.email]],
+          password: ['', [Validators.required, Validators.minLength(8)]]
+      });
+  }
+
+  onSubmit() {
+
+  }
 }
--- Authentication/src/app/Components/register-component/register-component.component.ts
+++ Authentication/src/app/Components/register-component/register-component.component.ts
@@ -1,12 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-component',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule,ReactiveFormsModule],
   templateUrl: './register-component.component.html',
   styleUrl: './register-component.component.css'
 })
 export class RegisterComponentComponent {
+  signupForm: FormGroup;
+  submitted = false;
 
+  constructor(private formBuilder: FormBuilder) {
+      this.signupForm = this.formBuilder.group({
+          fullName: ['', Validators.required],
+          email: ['', [Validators.required, Validators.email]],
+          password: ['', [Validators.required, Validators.minLength(8)]],
+          confirmPassword: ['', Validators.required],
+          role: ['', Validators.required],
+          terms: [false, Validators.requiredTrue]
+      })
+  }
+
+  onSubmit() {
+
+  }
 }
